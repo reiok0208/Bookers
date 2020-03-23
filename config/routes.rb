31e6cd04393544fc9e024ba851cb84e8791ba67f
REI OKADA <rei@REInoMacBook-Pro.local>
2020-03-23 19:30:00 +0900

    [Update]4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :books
+
+  get '/' => 'homes#top'
+
+  get 'books' => 'books#index'
+
+  post 'books' => 'books#create'
+
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
 end
